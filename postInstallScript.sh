5818309af696ba391c83cb9e0eb791b919f3171f
Abeer Ahmed <abeer.ahmed576@gmail.com>
2024-02-03 18:25:16 +0600

    Add files via upload

---
+++ postInstallScript.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+## Using desired timezone
+region=Asia
+city=Dhaka
+
+ln -sf /usr/share/zoneinfo/$region/$city /etc/localtime
+
+## Syncing system to hardware clock using UTC format
+hwclock --systohc --utc
+
+## Changing some pacman configuration using sed
+sed -i -e '/Color/s/#//' -e '/ParallelDownloads/s/#//' -e '/Color/a ILoveCandy' > /etc/pacman.conf
+
+## Enabling Bangla(BD) and English(US) locale
+sed -i -e '/bn_BD/s/#//' -e '/en_US.UTF-8/s/#//' > /etc/locale.gen
+
+locale-gen
+
+## Setting English(US) as system language
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+
+## Setting the hostname
+system_hostname=rajshahi-home
+
+echo $system_hostname > /etc/hostname
+
+## Configuring for localhost
+echo -e "\n127.0.0.1	localhost\n
+## For IPv6
+::1		localhost\n
+127.0.0.1	$system_hostname" >> /etc/hosts
+
+## Installing additional packages (with KDE)
+pacman -Syy
+
+pkglist=/home/myArchLinux/pkglist.txt
+pacman -S --needed - < $pkglist
+
+## Installing systemd-boot
+bootctl install
+
+## Defining minimum necessary kernel options in /etc/kernel/cmdline for kernel-install to use when making bootloader entries. This is also necessary for installation in a chroot as kernel-install will pick up kernel options of the live ISO's kernel which we dont' want.
+root_part=$(bootctl -R)
+root_uuid=$(blkid -o value -s UUID $root_part)
+machineID=$(cat /etc/machine-id)
+
+# The "rootflags" option is only needed for btrfs. If using ext2/3/4, remove this option.
+echo "root=UUID=$root_uuid rootflags=subvol=@ rw loglevel=3 systemd.machine_id=$machineID" > /etc/kernel/cmdline
+
+## Using kernel-install for placing kernel and initramsfs images at the required places for systemd-boot and automatic bootloader entry.
+kernel_vers=$(ls /usr/lib/modules)
+
+for version in ${kernel_vers[@]}; do
+    kernel-install add "$version" /usr/lib/modules/"$version"/vmlinuz
+    done
+
+## Cleanup /boot as kernel and initramsfs images created by mkinitcpio in this directory are useless for systemd-boot
+rm /boot/vmlinuz-linux* /boot/initramfs-linux*
+
+## Setting password for root account
+echo "Password for Root"
+passwd root
+
+## Creating a user account and setting password for it
+username=abeer
+## Write full name in quotes
+full_name="Abeer Ahmed"
+
+useradd -m -G wheel -c "$full_name" $username
+
+echo "Password for $username"
+passwd $username
+
+## Giving all memebers of wheel group privilege to execute any command
+echo "%wheel ALL=(ALL) ALL" > /etc/sudoers.d/all-for-wheel
+
+## Enabling necessary systemd services
+systemctl enable avahi-daemon
+systemctl enable haveged.service
+#systemctl enable bluetooth.service
+#systemctl enable cups.service
+systemctl enable fstrim.timer
+systemctl enable sddm.service
+systemctl enable NetworkManager
+systemctl enable upower
+#systemctl enable sshd
+#systemctl enable reflector.timer
+#systemctl enable firewalld
+
+## Making links of folders in personal files partition to ~/
+folders=$(ls /MyFiles)
+
+for folder in ${folders[@]}; do
+    ln -s /MyFiles/"$folder" /home/$username/"$folder"
+    done
